5ed69d973e6679c713daa798ce7fd7e448f09ea2
liuixaolei <13728817143@163.com>
2024-10-23 11:00:37 +0800

    fix: 🐛 移除废弃依赖

--- app/extensions.py
+++ app/extensions.py
@@ -1,7 +1,5 @@
 from flask_jwt_extended import JWTManager
 from flask_pymongo import PyMongo
-from flask_login import LoginManager
 
 mongo = PyMongo()
-login_manager = LoginManager()
 jwt = JWTManager()
